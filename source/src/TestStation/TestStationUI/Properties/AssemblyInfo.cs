761788e7c150b1f69b8e36963452b0d121bedb1b
= <=>
2020-04-03 17:41:12 +0800

    修改TestFlow软件的程序集名称

--- source/src/TestStation/TestStationUI/Properties/AssemblyInfo.cs
+++ source/src/TestStation/TestStationUI/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("EasyTest")]
-[assembly: AssemblyDescription("EasyTest application")]
+[assembly: AssemblyTitle("TestFlowSoftware")]
+[assembly: AssemblyDescription("TestFlow Software")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("JXI")]
-[assembly: AssemblyProduct("TestStation")]
+[assembly: AssemblyCompany("JYTEK")]
+[assembly: AssemblyProduct("TestFlow")]
 [assembly: AssemblyCopyright("Copyright ©2004-2019 JX Instrumentation")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
