8fef44163876f45a68b91bdebc219351ea51a346
jingtao <tao.jing@jytek.com>
2021-03-17 09:40:11 +0800

    修改WinformOperationPanelBase的编译错误

--- source/src/TestStation/ExternalLib/WinformCommonOi/WinformOperationPanelBase.cs
+++ source/src/TestStation/ExternalLib/WinformCommonOi/WinformOperationPanelBase.cs
@@ -13,7 +13,7 @@ namespace TestFlow.Software.WinformCommonOi
         /// <summary>
         /// OI面板可以开始运行的事件
         /// </summary>
-        public event Action<bool, string> OiReady;
+        public event Action<bool, Dictionary<string, object>> OiReady;
 
         public abstract void ShowPanel(ISequenceFlowContainer sequenceData, params object[] extraParams);
 
@@ -34,13 +34,24 @@ namespace TestFlow.Software.WinformCommonOi
         public abstract void SessionOver(ITestResultCollection sessionResult);
 
         public abstract void TestOver(IList<ITestResultCollection> testResults);
+        public void PrintMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowErrorMessage(bool isContinue, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Type ConfigPanelType { get; }
 
         public ISequenceFlowContainer SequenceData { get; }
         public OiSequenceType SupportSequence { get; }
 
         public void OnOiReady(bool oiInitSuccess, string message)
         {
-            this.OiReady?.Invoke(oiInitSuccess, message);
+            this.OiReady?.Invoke(oiInitSuccess, null);
         }
 
         public abstract void Dispose();
