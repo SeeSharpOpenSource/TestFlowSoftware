68b257617c99fa318c0b6c553017c0fbf85da3dd
jingtao <tao.jing@jytek.com>
2021-03-17 09:39:24 +0800

    提交9702的测试面板工程

---
+++ source/src/JY9702ProductTestPanel/JY9702ProductTestPanel.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace JY9702ProductTest
+{
+    partial class JY9702ProductTestPanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox_params = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button_start = new System.Windows.Forms.Button();
+            this.button_stop = new System.Windows.Forms.Button();
+            this.led1 = new SeeSharpTools.JY.GUI.LED();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox_params.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox_params
+            // 
+            this.groupBox_params.Controls.Add(this.comboBox1);
+            this.groupBox_params.Location = new System.Drawing.Point(12, 12);
+            this.groupBox_params.Name = "groupBox_params";
+            this.groupBox_params.Size = new System.Drawing.Size(312, 421);
+            this.groupBox_params.TabIndex = 0;
+            this.groupBox_params.TabStop = false;
+            this.groupBox_params.Text = "Parameters";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8"});
+            this.comboBox1.Location = new System.Drawing.Point(68, 60);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(222, 20);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(350, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(747, 421);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button_start
+            // 
+            this.button_start.Location = new System.Drawing.Point(12, 456);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(125, 53);
+            this.button_start.TabIndex = 2;
+            this.button_start.Text = "Start";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // button_stop
+            // 
+            this.button_stop.Location = new System.Drawing.Point(166, 456);
+            this.button_stop.Name = "button_stop";
+            this.button_stop.Size = new System.Drawing.Size(125, 53);
+            this.button_stop.TabIndex = 3;
+            this.button_stop.Text = "Stop";
+            this.button_stop.UseVisualStyleBackColor = true;
+            this.button_stop.Click += new System.EventHandler(this.button_stop_Click);
+            // 
+            // led1
+            // 
+            this.led1.BlinkColor = System.Drawing.Color.Lime;
+            this.led1.BlinkInterval = 1000;
+            this.led1.BlinkOn = false;
+            this.led1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.led1.Interacton = SeeSharpTools.JY.GUI.LED.InteractionStyle.Indicator;
+            this.led1.Location = new System.Drawing.Point(379, 456);
+            this.led1.Name = "led1";
+            this.led1.OffColor = System.Drawing.Color.Gray;
+            this.led1.OnColor = System.Drawing.Color.Lime;
+            this.led1.Size = new System.Drawing.Size(60, 60);
+            this.led1.Style = SeeSharpTools.JY.GUI.LED.LedStyle.Circular;
+            this.led1.TabIndex = 4;
+            this.led1.Value = false;
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(481, 456);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(237, 60);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // JY9702ProductTestPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1134, 535);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.led1);
+            this.Controls.Add(this.button_stop);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox_params);
+            this.Name = "JY9702ProductTestPanel";
+            this.Text = "JY9702 Product Test";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.JY9702ProductTestPanel_FormClosing);
+            this.groupBox_params.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox_params;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Button button_stop;
+        private SeeSharpTools.JY.GUI.LED led1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ source/src/JY9702ProductTestPanel/JY9702ProductTestPanel.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JY9702ProductTest
+{
+    public partial class JY9702ProductTestPanel : Form
+    {
+        public JY9702ProductTestPanel()
+        {
+            InitializeComponent();
+        }
+
+        public event Action<bool, Dictionary<string, object>> StartSequence;
+
+        public void ShowMessage(string message)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action(() =>
+                {
+                    this.textBox1.AppendText(message);
+                    this.textBox1.AppendText(Environment.NewLine);
+                }));
+            }
+            else
+            {
+                this.textBox1.AppendText(message);
+                this.textBox1.AppendText(Environment.NewLine);
+            }
+        }
+
+        public void ShowError(bool canStillRun, string message)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action(() =>
+                {
+                    MessageBox.Show(message, "Production Test", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }));
+            }
+            else
+            {
+                MessageBox.Show(message, "Production Test", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void SetStatus(bool isRunning, bool isError, bool isFailed)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action(() =>
+                {
+                    this.led1.BlinkOn = isRunning;
+                    if (isRunning)
+                    {
+                        this.label1.Text = "Running";
+                        this.label1.BackColor = Color.Orange;
+                    }
+                    else if (isError)
+                    {
+                        this.label1.Text = "Error";
+                        this.label1.BackColor = Color.Red;
+                    }
+                    else if (isFailed)
+                    {
+                        this.label1.Text = "Failed";
+                        this.label1.BackColor = Color.Red;
+                    }
+                    else
+                    {
+                        this.label1.Text = "Passed";
+                        this.label1.BackColor = Color.Green;
+                    }
+                }));
+            }
+            else
+            {
+                this.led1.BlinkOn = isRunning;
+                if (isError)
+                {
+                    this.label1.Text = "Error";
+                    this.label1.BackColor = Color.Red;
+                }
+                else if (isFailed)
+                {
+                    this.label1.Text = "Failed";
+                    this.label1.BackColor = Color.Red;
+                }
+                else
+                {
+                    this.label1.Text = "Passed";
+                    this.label1.BackColor = Color.Green;
+                }
+            }
+        }
+
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, object> data = new Dictionary<string, object>(10);
+            data.Add("SlotNumber", this.comboBox1.SelectedIndex);
+            StartSequence?.Invoke(true, data);
+        }
+
+        private void button_stop_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void JY9702ProductTestPanel_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            StartSequence?.Invoke(false, null);
+        }
+    }
+}
